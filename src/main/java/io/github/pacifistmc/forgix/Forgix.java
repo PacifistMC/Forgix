b800fbec040b99cac316c346a7062f0c22f8e4ce
hypherionmc <hypherionmc@gmail.com>
2024-05-03 07:13:11 +0200

    [DEV] Fix plugin trying to pull in depedendencies that are shadowed. Closes #27

--- src/main/java/io/github/pacifistmc/forgix/Forgix.java
+++ src/main/java/io/github/pacifistmc/forgix/Forgix.java
@@ -34,7 +34,7 @@ public class Forgix {
     public static final String manifestVersionKey = "Forgix-Version";
 
     public static class Merge {
-        private final String version = "1.2.8";
+        private final String version = "1.2.9";
         public static Set<PosixFilePermission> perms;
 
         static {
